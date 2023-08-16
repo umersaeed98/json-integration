1fb1567971b5fc70483032f18d8d87d2bcd01cc6
Omer Saeed <omersaeed@MP15-umerS-TSE-368.local>
2023-08-16 17:04:48 +0500

    umer-saeed-branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CommentsTable from './components/commentsTable/CommentsTable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import FilterInput from './components/filterInput/FilterInput';
 
 function App() {
-  const [filterId, setFilterId] = useState('');
 
-  const handleFilterChange = (event) => {
-    setFilterId(event.target.value);
-  };
+
 
   return (
     <div className="App">
-      <FilterInput value={filterId} onChange={handleFilterChange}/>
-      <CommentsTable filterId={filterId} />
+      <CommentsTable />
     </div>
   );
 }
--- src/components/filterInput/FilterInput.jsx
+++ src/components/filterInput/FilterInput.jsx
@@ -3,7 +3,13 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 
-const FilterInput = ({value,onChange}) => {
+const FilterInput = ({value,onEnterPress}) => {
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      onEnterPress(event.target.value);
+    }
+  };
   return (
     <Container className='d-flex flex-column align-items-center my-5 justify-content-center'>
          <label htmlFor="filterId"></label>
@@ -13,7 +19,7 @@ const FilterInput = ({value,onChange}) => {
         type="number"
         id="filterId"
         value={value}
-        onChange={onChange}
+        onKeyPress={handleKeyDown}
       />
     </Container>
       
